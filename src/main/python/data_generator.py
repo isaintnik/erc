return numpy, some fixes, project derivatives memory optimization, but not finished

src/main/python/data_generator.py
@@ -1,5 +1,4 @@
 import numpy as np
-import torch
 from src.main.python.model import UserLambda, USession, InteractionCalculator
 
 USER_IDX = 0
@@ -21,17 +20,17 @@ class StepGenerator:
         self.beta = beta
         self.verbose = verbose
         self.other_project_importance = other_project_importance
-        self.user_embedding = user_embedding if user_embedding is not None else torch.randn(dim)
-        self.project_embeddings = project_embeddings if project_embeddings is not None else torch.randn(n_projects, dim)
+        self.user_embedding = user_embedding if user_embedding is not None else np.random.randn(dim)
+        self.project_embeddings = project_embeddings if project_embeddings is not None else np.random.randn(n_projects,
+                                                                                                            dim)
         self.user_lambdas = UserLambda(user_embedding=self.user_embedding,
                                        n_projects=len(self.project_embeddings),
                                        beta=self.beta,
                                        other_project_importance=self.other_project_importance,
                                        derivative=False,
                                        square=False,
-                                       interactions_supplier=InteractionCalculator(self.user_embedding.unsqueeze(0),
-                                                                                   self.project_embeddings).
-                                       get_user_supplier(0))
+                                       interactions_supplier=InteractionCalculator(
+                                           [self.user_embedding], self.project_embeddings).get_user_supplier(0))
 
     def _select_project_at_current_time(self, projects_ids):
         time_deltas = {pid: np.random.exponential(scale=1 / self.user_lambdas.get(pid) ** 2) for pid in projects_ids}

src/main/python/model.py
@@ -3,121 +3,35 @@ import math
 from collections import namedtuple
 import warnings
 import numpy as np
-import torch
+from src.main.python.lambda_calc import *
 
 
 USession = namedtuple('USession', 'pid start_ts end_ts pr_delta n_tasks')
 
 
-def interaction_matrix(users, projects):
-    return users @ projects.t()
-
-
-class InteractionCalculator:
-    def __init__(self, user_embeddings, project_embeddings):
-        self.interactions = interaction_matrix(user_embeddings, project_embeddings)
-
-    def get_interaction(self, user_id, project_id):
-        return self.interactions[user_id, project_id].item()
-
-    def get_user_supplier(self, user_id):
-        return lambda project_id: self.interactions[user_id, project_id].item()
-
-
-class UserProjectLambda:
-    def __init__(self, user_embedding, n_projects, beta, interactions_supplier, *, derivative=False, square=False):
-        self.numerator = 10
-        self.denominator = 10
-        self.beta = beta
-        # self.square = square
-        self.derivative = derivative
-        self.user_embedding = user_embedding
-        self.user_d_numerator = torch.zeros_like(user_embedding)
-        self.project_d_numerators = torch.zeros(n_projects, len(user_embedding))
-        self.avg_time_between_sessions = 5
-        self.interactions_supplier = interactions_supplier
-        self.lambda_project_id = None
-
-    def set_project_id(self, project_id):
-        self.lambda_project_id = project_id
-
-    def update(self, project_embedding, session_project_id, n_tasks, delta_time, coeff):
-        e = np.exp(-self.beta)  # * delta_time)
-        ua = self.interactions_supplier(session_project_id)
-        self.numerator = e * self.numerator + coeff * ua * n_tasks  # * (ua if self.square else 1)
-        self.denominator = e * self.denominator + coeff
-        if self.derivative:
-            self.user_d_numerator = e * self.user_d_numerator + coeff * project_embedding * n_tasks  # * (2 * ua if self.square else 1)
-            self.project_d_numerators *= e
-            if self.lambda_project_id is not None:
-                self.project_d_numerators[self.lambda_project_id] += coeff * self.user_embedding * n_tasks  # * (2 * ua if self.square else 1)
-
-    def get(self):
-        cur_lambda = self.numerator / self.denominator / self.avg_time_between_sessions
-        if self.derivative:
-            user_derivative = self.user_d_numerator / self.denominator / self.avg_time_between_sessions
-            project_derivative = self.project_d_numerators / self.denominator / self.avg_time_between_sessions
-            if math.isnan(user_derivative[0]):
-                print(self.denominator)
-            if math.isnan(user_derivative[0]):
-                print(self.user_d_numerator)
-            # if math.isnan(project_derivative[0]):
-            #     print(self.project_d_numerators)
-            return cur_lambda, user_derivative, project_derivative
-        return cur_lambda
-
-
-class UserLambda:
-    def __init__(self, user_embedding, n_projects, beta, other_project_importance, interactions_supplier,
-                 derivative=False, square=False):
-        self.project_lambdas = {}
-        self.beta = beta
-        self.derivative = derivative
-        self.other_project_importance = other_project_importance
-        self.user_embedding = user_embedding
-        if math.isnan(user_embedding[0]):
-            print("u_e is nan")
-        self.default_lambda = UserProjectLambda(self.user_embedding, n_projects, self.beta, interactions_supplier,
-                                                derivative=derivative, square=square)
-
-    def update(self, project_embedding, session, delta_time):
-        if session.pid not in self.project_lambdas:
-            self.project_lambdas[session.pid] = copy.deepcopy(self.default_lambda)
-            self.project_lambdas[session.pid].set_project_id(session.pid)
-        for project_id, project_lambda in self.project_lambdas.items():
-            coefficient = 1 if project_id == session.pid else self.other_project_importance
-            project_lambda.update(project_embedding, session.pid, session.n_tasks, delta_time, coefficient)
-        self.default_lambda.update(project_embedding, session.pid, session.n_tasks,
-                                   delta_time, self.other_project_importance)
-
-    def get(self, project_id):
-        if project_id not in self.project_lambdas.keys():
-            return self.default_lambda.get()
-        return self.project_lambdas[project_id].get()
-
-
 class Model:
     def __init__(self, users_history, dim, learning_rate=0.003, beta=0.001, eps=3600, other_project_importance=0.3,
-                 users_embeddings_prior=None, projects_embeddings_prior=None, square=False, device='cpu'):
+                 users_embeddings_prior=None, projects_embeddings_prior=None, square=False):
         self.users_history = users_history
         self.emb_dim = dim
         self.learning_rate = learning_rate
         self.decay_rate = 0.97
         self.beta = beta
         self.eps = eps
+        self.square = square
         self.data_size = sum([len(user) for user in users_history])
         self.other_project_importance = other_project_importance
-        self.square = square
-        self.device = device
 
-        self.user_embeddings = users_embeddings_prior if users_embeddings_prior is not None \
-            else torch.randn(len(self.users_history), self.emb_dim) * 0.2
+        self.user_embeddings = np.array(
+            [np.random.normal(0, 0.2, self.emb_dim) for _ in range(len(self.users_history))]) \
+            if users_embeddings_prior is None else users_embeddings_prior
         if projects_embeddings_prior is None:
             projects_set = set()
             for user_history in self.users_history:
                 for session in user_history:
                     projects_set.add(session.pid)
-            self.project_embeddings = torch.randn(len(projects_set), self.emb_dim) * 0.2
+            self.project_embeddings = np.array(
+                [np.random.normal(0, 0.2, self.emb_dim) for _ in range(len(projects_set))])
         else:
             self.project_embeddings = projects_embeddings_prior
 
@@ -129,8 +43,8 @@ class Model:
 
     def _likelihood_derivative(self, derivative=False):
         ll = 0.
-        users_derivatives = torch.zeros_like(self.user_embeddings)
-        project_derivatives = torch.zeros_like(self.project_embeddings)
+        users_derivatives = np.zeros_like(self.user_embeddings)
+        project_derivatives = np.zeros_like(self.project_embeddings)
         interaction_calculator = InteractionCalculator(self.user_embeddings, self.project_embeddings)
         for user_id in range(len(self.users_history)):
             user_history = self.users_history[user_id]
@@ -196,14 +110,14 @@ class Model:
 
 class Model2UA(Model):
     def __init__(self, users_history, dim, learning_rate=0.01, beta=0.01, eps=3600, other_project_importance=0.5,
-                 users_embeddings_prior=None, projects_embeddings_prior=None, device='cpu'):
+                 users_embeddings_prior=None, projects_embeddings_prior=None):
         Model.__init__(self, users_history, dim, learning_rate, beta, eps, other_project_importance,
-                       users_embeddings_prior, projects_embeddings_prior, square=True, device=device)
+                       users_embeddings_prior, projects_embeddings_prior, square = True)
 
     def _session_likelihood(self, user_session, user_lambda):
         cur_lambda = user_lambda.get(user_session.pid)
-        return torch.log(-torch.exp(-cur_lambda * (user_session.pr_delta + self.eps)) +
-                         torch.exp(-cur_lambda * (user_session.pr_delta - self.eps)))
+        return np.log(-np.exp(-cur_lambda * (user_session.pr_delta + self.eps)) +
+                      np.exp(-cur_lambda * (user_session.pr_delta - self.eps)))
 
     def _last_likelihood(self, user_session, user_lambda):
         return -user_lambda.get(user_session.pid) * user_session.pr_delta
@@ -211,9 +125,9 @@ class Model2UA(Model):
     def _update_session_derivative(self, user_session, user_id, user_lambda, users_derivatives, project_derivatives):
         lam, lam_user_d, lam_projects_d = user_lambda.get(user_session.pid, derivative=True)
         tau = user_session.pr_delta
-        cur_ll_d = ((tau + self.eps) * torch.exp(-lam * (tau + self.eps)) -
-                    max(0, tau - self.eps) * torch.exp(-lam * max(0, tau - self.eps))) / \
-                   (-torch.exp(-lam * (tau + self.eps)) + torch.exp(-lam * max(0, tau - self.eps)))
+        cur_ll_d = ((tau + self.eps) * np.exp(-lam * (tau + self.eps)) -
+                    max(0, tau - self.eps) * np.exp(-lam * max(0, tau - self.eps))) / \
+                   (-np.exp(-lam * (tau + self.eps)) + np.exp(-lam * max(0, tau - self.eps)))
         users_derivatives[user_id] += cur_ll_d * lam_user_d
         project_derivatives[user_session.pid] += cur_ll_d * lam_projects_d
 
@@ -225,9 +139,9 @@ class Model2UA(Model):
 
 class Model2Lambda(Model):
     def __init__(self, users_history, dim, learning_rate=0.01, beta=0.01, eps=3600, other_project_importance=0.5,
-                 users_embeddings_prior=None, projects_embeddings_prior=None, device='cpu'):
+                 users_embeddings_prior=None, projects_embeddings_prior=None):
         Model.__init__(self, users_history, dim, learning_rate, beta, eps, other_project_importance,
-                       users_embeddings_prior, projects_embeddings_prior, square=False, device=device)
+                       users_embeddings_prior, projects_embeddings_prior, square = False)
 
     def _session_likelihood(self, user_session, user_lambda):
         cur_lambda2 = user_lambda.get(user_session.pid) ** 2
@@ -280,9 +194,10 @@ class Model2Lambda(Model):
 
 class ModelExpLambda(Model):
     def __init__(self, users_history, dim, learning_rate=0.01, beta=0.01, eps=3600, other_project_importance=0.5,
-                 device='cpu'):
-        Model.__init__(self, users_history, dim, learning_rate, beta, eps, other_project_importance, square=False,
-                       device=device)
+                 users_embeddings_prior=None, projects_embeddings_prior=None):
+        Model.__init__(self, users_history, dim, learning_rate, beta, eps, other_project_importance,
+                       users_embeddings_prior, projects_embeddings_prior, square = False)
+        self.square = False
 
     def _session_likelihood(self, user_session, user_lambda):
         exp_lambda = np.exp(user_lambda.get(user_session.pid))

src/test/python/basic_synthetic.py
@@ -1,6 +1,4 @@
 import time
-import argparse
-import torch
 from src.main.python.model import *
 from src.main.python.data_generator import *
 
@@ -15,8 +13,8 @@ def generate_synthetic():
 
 
 def generate_vectors(users_num, projects_num, dim, std_dev=0.2):
-    return torch.randn(users_num, dim) * std_dev + 0.5, \
-           torch.randn(projects_num, dim) * std_dev + 0.5
+    return np.array([np.random.normal(0.5, std_dev, dim) for _ in range(users_num)]), \
+           np.array([np.random.normal(0.5, std_dev, dim) for _ in range(projects_num)])
 
 
 def vecs_dist(vecs1, vecs2):
@@ -24,6 +22,10 @@ def vecs_dist(vecs1, vecs2):
     for i in range(len(vecs1)):
         sum += abs(vecs1[i] - vecs2[i])
     return sum
+
+
+def interaction_matrix(users, projects):
+    return users @ projects.T
 
 
 def correct_likelihood_test():
@@ -114,13 +116,13 @@ def synthetic_test():
     print(np.linalg.norm(start_interaction - end_interaction))
 
 
-def init_compare_test(no_cuda=False):
-    users_num = 10
-    projects_num = 10
+def init_compare_test():
+    users_num = 5
+    projects_num = 3
     dim = 2
     beta = 0.001
     other_project_importance = 0.3
-    learning_rate = 0.9
+    learning_rate = 0.1
     iter_num = 36
     users, projects = generate_vectors(users_num, projects_num, dim, std_dev=0.2)
     X = [StepGenerator(user_embedding=user, project_embeddings=projects, beta=beta,
@@ -128,23 +130,20 @@ def init_compare_test(no_cuda=False):
              .generate_user_steps() for user in users]
     print(len(X[0]))
     print("data generated")
-    device = 'cuda' if not no_cuda and torch.cuda.is_available() else 'cpu'
-    m1_init_users, m1_init_projects = generate_vectors(users_num, projects_num, dim)
     model1 = Model2Lambda(X, dim, learning_rate=learning_rate, eps=20, beta=beta,
                           other_project_importance=other_project_importance,
-                          users_embeddings_prior=m1_init_users,
-                          projects_embeddings_prior=m1_init_projects,
-                          device=device)
+                          users_embeddings_prior=np.array([np.random.normal(0.5, 0.2, dim)
+                                                           for _ in range(users_num)]),
+                          projects_embeddings_prior=np.array([np.random.normal(0.5, 0.2, dim)
+                                                             for _ in range(projects_num)]))
     model2 = Model2Lambda(X, dim, learning_rate=learning_rate, eps=20, beta=beta,
                           other_project_importance=other_project_importance,
                           users_embeddings_prior=users,
-                          projects_embeddings_prior=projects,
-                          device=device)
+                          projects_embeddings_prior=projects)
     start_interaction = interaction_matrix(users, projects)
     init_interaction = interaction_matrix(model1.user_embeddings, model1.project_embeddings)
     for i in range(iter_num):
         if i % 5 == 0 or i in [1, 2]:
-        # if i in [0, 1, 2, 5, 7, 10, 15, 20]:
             end_interaction1 = interaction_matrix(model1.user_embeddings, model1.project_embeddings)
             end_interaction2 = interaction_matrix(model2.user_embeddings, model2.project_embeddings)
             inter_norm1 = np.linalg.norm(end_interaction1 - start_interaction)
@@ -152,33 +151,28 @@ def init_compare_test(no_cuda=False):
             print("{}-th iter, ll = {}, inter_norm = {}".format(i, model1.log_likelihood(), inter_norm1))
             print("{}-th iter, ll = {}, inter_norm = {}".format(i, model2.log_likelihood(), inter_norm2))
             print("|m1 - m2| = {}, |m1*c - s| = {}".format(np.linalg.norm(end_interaction2 - end_interaction1),
-                  torch.norm(torch.mean(start_interaction / end_interaction1) * end_interaction1 - start_interaction)))
+                  np.linalg.norm(np.mean(start_interaction / end_interaction1) * end_interaction1 - start_interaction)))
             print()
         model1.optimization_step()
         model2.optimization_step()
     end_interaction1 = interaction_matrix(model1.user_embeddings, model1.project_embeddings)
     end_interaction2 = interaction_matrix(model2.user_embeddings, model2.project_embeddings)
-    print("|start - init| =", torch.norm(start_interaction - init_interaction))
-    print("|start - end1| =", torch.norm(start_interaction - end_interaction1))
-    print("|start - end2| =", torch.norm(start_interaction - end_interaction2))
-    print("coeff =", torch.mean(end_interaction1 / start_interaction))
+    print("|start - init| =", np.linalg.norm(start_interaction - init_interaction))
+    print("|start - end1| =", np.linalg.norm(start_interaction - end_interaction1))
+    print("|start - end2| =", np.linalg.norm(start_interaction - end_interaction2))
+    print("coeff =", np.mean(end_interaction1 / start_interaction))
     print(start_interaction)
     print(end_interaction1)
     print(end_interaction2)
     print()
-    print(torch.norm(torch.mean(start_interaction / end_interaction1) * end_interaction1 - start_interaction))
+    print(np.linalg.norm(np.mean(start_interaction / end_interaction1) * end_interaction1 - start_interaction))
 
 
 if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--no_cuda', action='store_true')
-    args = arg_parser.parse_args()
-
     np.random.seed(3)
-    torch.manual_seed(3)
     start_time = time.time()
     # correct_derivative_test()
     # convergence_test()
     # synthetic_test()
-    init_compare_test(args.no_cuda)
+    init_compare_test()
     print("time:", time.time() - start_time)